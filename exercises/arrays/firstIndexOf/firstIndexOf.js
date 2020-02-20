634f91e409f5d078c675fb37e70aba61c322e32f
Arian <razzaghi2012@gmail.com>
2020-02-20 13:48:16 -0800

    pushing before my break for the firstIndexOf... in the comments i've left my logic

--- exercises/arrays/firstIndexOf/firstIndexOf.js
+++ exercises/arrays/firstIndexOf/firstIndexOf.js
@@ -15,15 +15,48 @@
  * @returns {boolean} The index of the first occurrence of the value in the
  *  array, or -1 if it's not found.
  */
+
+ /** Arian's thoughts
+1. you have to iterate thru an array
+2. you have to use the anchor as a way to find what you're looking for
+3. if the loop finds the value from the anchor you gave it, then it will return the position of that value in the loop
+4. so we have to return something to the extent of the position of the array
+5. else if we don't see this we must return -1
+  */
+
+
 function firstIndexOf(haystack, needle) {
-  // This is your job. :)
+  for (let i = 0 ; i < haystack.length ; i++ ) {
+    let currentValue = haystack[i];
+    console.log (currentValue);
+    if (needle === currentValue) {
+      return haystack.indexOf(needle);
+    }
+    else {
+      return -1;
+    }
+  }
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for firstIndexOf:');
 
+  //sanity 1 --- should return ==> 2// 
+  console.log(firstIndexOf(['apple', 'pie','love'],'love'));
+
+  //sanity 2 --- should return ==> -1//
+  console.log(firstIndexOf(['apple', 'pie','love'],'nada'));
+
+
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
+
+/**
+ * checking my sanity here
+ * i know this isn't working because i should not be getting -1 for both sanity checks. My guess is that there is something wrong with the operant
+ * i see from the terminal that console.log (currentValue) ==> apple so it doesn't seem to be iterating through the loop?
+ * maybe my loop inst working because in the terminal i don't see each value of apple, pie, love come out. 
+ */
 
 module.exports = firstIndexOf;
